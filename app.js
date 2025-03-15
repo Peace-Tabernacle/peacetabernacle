957ad7829fe57c06ed568beea5e4e0f52ebfdf32
Peace-Tabernacle <peacetabernacle@2016@gmail.com>
2025-03-15 22:37:18 +0200

    Updates in app.jss

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menu = document.querySelector('#mobile-menu'); // Corrected selector
-    const menuLinks = document.querySelector('.navbar__menu');
+// document.addEventListener("DOMContentLoaded", function() {
+//     const menu = document.querySelector('#mobile-menu'); // Corrected selector
+//     const menuLinks = document.querySelector('.navbar__menu');
 
-    if (menu && menuLinks) { // Ensuring elements exist
-        menu.addEventListener('click', function() {
-            menu.classList.toggle('is-active');
-            menuLinks.classList.toggle('active');
-        });
-    }
-});
+//     if (menu && menuLinks) { // Ensuring elements exist
+//         menu.addEventListener('click', function() {
+//             menu.classList.toggle('is-active');
+//             menuLinks.classList.toggle('active');
+//         });
+//     }
+// });
 document.addEventListener("DOMContentLoaded", function() {
     const mobileMenu = document.getElementById("mobile-menu");
     const navMenu = document.querySelector(".navbar__menu");

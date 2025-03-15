ce0e920eaa64314b736c2969675416fc0c8d145a
Peace-Tabernacle <peacetabernacle@2016@gmail.com>
2025-03-15 23:37:14 +0200

    Updates in index.html style.css app.js

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const menu = document.querySelector('#mobile-menu'); // Corrected selector
-//     const menuLinks = document.querySelector('.navbar__menu');
-
-//     if (menu && menuLinks) { // Ensuring elements exist
-//         menu.addEventListener('click', function() {
-//             menu.classList.toggle('is-active');
-//             menuLinks.classList.toggle('active');
-//         });
-//     }
-// });
 document.addEventListener("DOMContentLoaded", function() {
-    const mobileMenu = document.getElementById("mobile-menu");
-    const navMenu = document.querySelector(".navbar__menu");
+    const menu = document.querySelector('#mobile-menu'); // Corrected selector
+    const menuLinks = document.querySelector('.navbar__menu');
 
-    mobileMenu.addEventListener("click", function() {
-        navMenu.classList.toggle("active");
-
-        // Toggle animation for bars
-        mobileMenu.classList.toggle("is-active");
-    });
+    if (menu && menuLinks) { // Ensuring elements exist
+        menu.addEventListener('click', function() {
+            menu.classList.toggle('is-active');
+            menuLinks.classList.toggle('active');
+        });
+    }
 });
